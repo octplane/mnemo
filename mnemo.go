c7185ffe1d53842d4af81d24365601e0984e9822
Pierre Baillet <oct@fotonauts.com>
2013-07-05 16:25:10 +0200

    WIP Implementation

--- mnemo.go
+++ mnemo.go
@@ -32,7 +32,7 @@ func SPECIALS() map[string]string {
 		"si": "shi",
 		"ti": "chi",
 		"tu": "tsu",
-		"zi": "tzu"
+		"zi": "tzu",
 	}
 }
 
@@ -52,12 +52,37 @@ func toInt(value string) int {
 	if strings.HasPrefix(value, NEG) {
 		return -1 * toInt(value[len(NEG):])
 	}
-	n, _ := toNumber(value[len(value)-2 : 2])
+	n, _ := toNumber(value[len(value)-2 : len(value)])
+
 	return len(SYL())*toInt(value[0:len(value)-3]) + n
 }
 
 func toSpecial(value string) string {
+	for s, d := range SPECIALS() {
+		value = strings.Replace(value, s, d, -1)
+	}
+	return value
+}
 
+func fromSpecial(value string) string {
+	for d, s := range SPECIALS() {
+		value = strings.Replace(value, s, d, -1)
+	}
+	return value
+}
+
+func fromInteger(value int) string {
+	if value == 0 {
+		return ""
+	}
+	mod := value % len(SYL())
+	rest := value / len(SYL())
+
+	return FromInteger(rest) + SYL()[mod]
+}
+
+func IsMnemoWord(value string) bool {
+	return true
 }
 
 func FromInteger(value int) string {
@@ -68,6 +93,10 @@ func FromInteger(value int) string {
 	return toSpecial(fromInteger(value))
 }
 
+func ToInteger(value string) int {
+	return toInt(fromSpecial(value))
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(FromInteger(12))
 }
--- mnemo_test.go
+++ mnemo_test.go
@@ -1,9 +1,29 @@
 package mnemo
 
 import (
+	"fmt"
 	"testing"
 )
 
+func TestSpecial(t *testing.T) {
+	if toSpecial("hu") != "fu" {
+		t.Fatalf("%s should be special of %s, got %s", "fu", "hu", toSpecial("hu"))
+	}
+
+	if fromSpecial("fu") != "hu" {
+		t.Fatalf("%s should be from special of %s, got %s", "hu", "fu", fromSpecial("fu"))
+	}
+}
+
+func TestBackAndForth(t *testing.T) {
+	fmt.Printf("takeshi = %d\n", ToInteger("takeshi"))
+	fmt.Printf("%d = %s\n", ToInteger("takeshi"), FromInteger(ToInteger("takeshi")))
+}
+
 func TestIsMnemoWord(t *testing.T) {
+	if !IsMnemoWord("takeshi") {
+		t.Fatalf("%s is not a word and should be", "takeshi")
+
+	}
 
 }
